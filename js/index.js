1af66f157189e7c6addd8935bd260480f1a3591c
sgq456 <1091306458@qq.com>
2016-10-21 07:31:36 +0800

    报名通道
    报名通道

--- js/index.js
+++ js/index.js
@@ -4,11 +4,33 @@ $(function () {
         onlyExternal : true,
         speed:500
     });
-    $(".sgq_public_item .sgq_pubulic_menu").on('touchstart',function(e){
+    $(".sgq_result .sgq_public_item .sgq_pubulic_menu").on('touchstart',function(e){
+        e.preventDefault();
+        $(this).parent().children(".hot").removeClass('hot');
+        $(this).addClass('hot');
+        shixunSwiper.swipeTo( $(this).index() );
+    });
+    //就业成果
+    var workSwiper = new Swiper('.sgq_work_box',{
+        onlyExternal : true,
+        speed:500
+    });
+    $(".sgq_work .sgq_public_item .sgq_pubulic_menu").on('touchstart',function(e){
         e.preventDefault()
         $(this).parent().children(".hot").removeClass('hot');
         $(this).addClass('hot')
-        shixunSwiper.swipeTo( $(this).index() )
+        workSwiper.swipeTo( $(this).index() );
+    });
+    //优逸新闻
+    var newsSwiper = new Swiper('.sgq_news_box',{
+        onlyExternal : true,
+        speed:500
+    });
+    $(".sgq_news .sgq_public_item .sgq_pubulic_menu").on('touchstart',function(e){
+        e.preventDefault()
+        $(this).parent().children(".hot").removeClass('hot');
+        $(this).addClass('hot')
+        newsSwiper.swipeTo( $(this).index() );
     });
     
     
