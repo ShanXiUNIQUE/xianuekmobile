3224e26080cbc7ff7213ff28f938e8ed0f3a09df
sgq456 <1091306458@qq.com>
2016-10-20 20:32:11 +0800

    就业成果
    就业成果

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
 $(function () {
-    
+    //实训效果
+    var shixunSwiper = new Swiper('.sgq_result_box',{
+        onlyExternal : true,
+        speed:500
+    });
+    $(".sgq_public_item .sgq_pubulic_menu").on('touchstart',function(e){
+        e.preventDefault()
+        $(this).parent().children(".hot").removeClass('hot');
+        $(this).addClass('hot')
+        shixunSwiper.swipeTo( $(this).index() )
+    });
     
     
     
