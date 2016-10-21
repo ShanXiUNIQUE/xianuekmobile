aa733296e675e066daea47a16a6724a1af512779
sgq456 <1091306458@qq.com>
2016-10-22 01:01:13 +0800

    底部
    底部

--- js/index.js
+++ js/index.js
@@ -1,3 +1,89 @@
+(function($) {
+    var DEFAULTS = {
+        duration: 200,
+        updateRate: 15
+    };
+
+    var interpolate = function (source, target, shift) {
+        return (source + (target - source) * shift);
+    };
+
+    var easing = function (pos) {
+        return (-Math.cos(pos * Math.PI) / 2) + .5;
+    };
+
+    var scroll = function(settings) {
+        var options = $.extend({}, DEFAULTS, settings);
+
+        if (options.duration === 0) {
+            window.scrollTo(0, options.endY);
+            if (typeof options.callback === 'function') options.callback();
+            return;
+        }
+
+        var startY = window.pageYOffset,
+            startT = Date.now(),
+            finishT = startT + options.duration;
+
+        var animate = function() {
+            var now = Date.now(),
+                shift = (now > finishT) ? 1 : (now - startT) / options.duration;
+
+            window.scrollTo(0, interpolate(startY, options.endY, easing(shift)));
+
+            if (now < finishT) {
+                setTimeout(animate, options.updateRate);
+            }
+            else {
+                if (typeof options.callback === 'function') options.callback();
+            }
+        };
+
+        animate();
+    };
+
+    var scrollNode = function(settings) {
+        var options = $.extend({}, DEFAULTS, settings);
+
+        if (options.duration === 0) {
+            this.scrollTop = options.endY;
+            if (typeof options.callback === 'function') options.callback();
+            return;
+        }
+
+        var startY = this.scrollTop,
+            startT = Date.now(),
+            finishT = startT + options.duration,
+            _this = this;
+
+        var animate = function() {
+            var now = Date.now(),
+                shift = (now > finishT) ? 1 : (now - startT) / options.duration;
+
+            _this.scrollTop = interpolate(startY, options.endY, easing(shift));
+
+            if (now < finishT) {
+                setTimeout(animate, options.updateRate);
+            }
+            else {
+                if (typeof options.callback === 'function') options.callback();
+            }
+        };
+
+        animate();
+    };
+
+    $.scrollTo = scroll;
+
+    $.fn.scrollTo = function() {
+        if (this.length) {
+            var args = arguments;
+            this.forEach(function(elem, index) {
+                scrollNode.apply(elem, args);
+            });
+        }
+    };
+}(Zepto));
 $(function () {
     //实训效果
     var shixunSwiper = new Swiper('.sgq_result_box',{
@@ -34,11 +120,12 @@ $(function () {
     });
 //    返回顶部
 
-    touch.on( $('.sgq_backTop'),"doubleTap", function () {
-        $("html,body").animate({
-            scrollTop:0
-        }, 500)
-    } );
+    $('.sgq_backTop').on('doubletap', function(e) {
+        $.scrollTo({
+            endY: 0,
+            duration: 200
+        });
+    });
     
     
     
